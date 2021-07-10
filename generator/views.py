c5133ee23b1a83c9074ef32ed8e94863298288ca
Ganiyat <ganiyatolajumoke.go@gmail.com>
2021-07-10 11:22:18 +0100

    added an option to select the length of password from a dropdown

--- generator/views.py
+++ generator/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def HomeView(request):
+    context = {}
+    return render(request, 'home.html', context)
+
+def PasswordView(request):
     context = {
         "password": "aiugfjseuihuiiay",
         "username": "ghina"
     }
-    return render(request, 'home.html', context)
-
-def PasswordView(request):
-    context = {}
     return render(request, 'password.html',context)
