7aae414c9343f5ced8333fed6b9238452e036324
Ganiyat <ganiyatolajumoke.go@gmail.com>
2021-07-10 11:10:56 +0100

    created a password view, url and also a template to display the view created

--- generator/urls.py
+++ generator/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import HomeView
-
+from .views import HomeView, PasswordView
 urlpatterns = [
     path('', HomeView, name='home'),
+    path('password/', PasswordView, name='password'),
 ]
--- generator/views.py
+++ generator/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def HomeView(request):
-    context = {}
+    context = {
+        "password": "aiugfjseuihuiiay",
+        "username": "ghina"
+    }
     return render(request, 'home.html', context)
+
+def PasswordView(request):
+    context = {}
+    return render(request, 'password.html',context)
